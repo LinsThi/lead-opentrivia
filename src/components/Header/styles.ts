ec303f106b62be8d8335a4daa686f93e879b56d9
LinsThi <thiagolins13255@gmail.com>
2021-12-13 17:21:13 -0300

    Fixing Header

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,6 +1,7 @@
 import styled from 'styled-components/native';
 
 import Icon from '../Icon';
+import { NewText } from '../Text';
 
 interface IconProps {
   name: string;
@@ -9,16 +10,29 @@ interface IconProps {
 
 interface ContainerProps {
   enableNavigation?: boolean;
-  headerLogin?: boolean;
 }
 
 export const Container = styled.View`
-  height: 50px;
   width: 100%;
   background: ${({ theme }) => theme.Colors.HEADER_COLOR};
+`;
+
+export const ContainerButtons = styled.View<ContainerProps>`
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 5px 10px;
+`;
+
+export const ContainerFont = styled.View`
+  flex-direction: row;
+`;
+
+export const ContainerHeaderDown = styled.View`
+  width: 100%;
+  height: 50px;
+  padding-top: 5px;
 
   flex-direction: row;
-  justify-content: center;
   align-items: center;
 `;
 
@@ -27,18 +41,27 @@ export const ContainerTitle = styled.View<ContainerProps>`
   margin-left: 10px;
 `;
 
-export const ContainerButtons = styled.View<ContainerProps>`
-  width: ${({ headerLogin }) => (headerLogin ? 100 : 7)}%;
-  flex-direction: row;
-  justify-content: space-between;
-`;
-
 export const ButtonLeft = styled.TouchableOpacity`
-  flex: 0.1;
-  margin-left: -10px;
+  margin-left: 10px;
 `;
 
 export const ButtonTheme = styled.TouchableOpacity``;
+
+export const RestoreFont = styled(NewText).attrs({
+  fontSize: 25,
+})`
+  font-weight: bold;
+  color: ${({ theme }) => theme.Colors.ICON_COLOR};
+  margin-right: 20px;
+  margin-left: 18px;
+`;
+
+export const DecreaseIncreaseFont = styled(NewText).attrs({
+  fontSize: 25,
+})`
+  font-weight: bold;
+  color: ${({ theme }) => theme.Colors.ICON_COLOR};
+`;
 
 export const IconLeft = styled(Icon).attrs<IconProps>(
   ({ name, iconType, theme }) => ({
