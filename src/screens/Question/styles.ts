40d511cdd3adc3b9b87fb41c602f04742647a1a8
LinsThi <thiagolins13255@gmail.com>
2021-12-20 15:52:51 -0300

    Fix CSS

--- src/screens/Question/styles.ts
+++ src/screens/Question/styles.ts
@@ -4,7 +4,7 @@ import styled from 'styled-components/native';
 const { width } = Dimensions.get('window');
 
 interface ButtonProps {
-  buttonBG: string;
+  isDisabled?: boolean;
 }
 
 export const Container = styled.View`
@@ -33,8 +33,8 @@ export const ContainerAskTitle = styled.View`
 
 export const ContainerButtons = styled.View`
   width: 100%;
-  height: 50px;
-  padding: 0px 30px;
+  height: 70px;
+  padding: 12px 30px;
 
   flex-direction: row;
   justify-content: space-between;
@@ -47,7 +47,12 @@ export const ButtonOption = styled.TouchableOpacity`
 export const ButtonNavigation = styled.TouchableOpacity<ButtonProps>`
   padding: 5px 5px;
   justify-content: center;
-  border: 1px ${({ buttonBG }) => buttonBG};
+  border: 1px
+    ${({ isDisabled, theme }) =>
+      isDisabled
+        ? theme.Colors.BUTTON_DISABLE_COLOR
+        : theme.Colors.BORDER_BUTTON_COLOR};
   border-radius: 10px;
-  background: ${({ buttonBG }) => buttonBG};
+  background: ${({ isDisabled, theme }) =>
+    isDisabled ? theme.Colors.BUTTON_DISABLE_COLOR : theme.Colors.BUTTON_COLOR};
 `;
